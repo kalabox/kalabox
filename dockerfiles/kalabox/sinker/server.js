fad3e936be158b747d6e33d6ea37a958d6aa2db0
Mike Milano <coder1@gmail.com>
2014-09-15 06:14:12 -0700

    removing old dockerfiles

--- dockerfiles/kalabox/sinker/server.js
+++
@@ -1,9 +0,0 @@
-var sinker = require('./sinker');
-var net = require('net');
-var dir = process.argv[2];
-
-var server = net.createServer(function (stream) {
-    var sink = sinker(dir);
-    sink.pipe(stream).pipe(sink);
-});
-server.listen(5000);
