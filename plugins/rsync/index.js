49005f8ca33f575529ac0b06fd28263d96333a38
Mike Milano <coder1@gmail.com>
2014-09-18 12:23:41 -0700

    rsync suport

---
+++ plugins/rsync/index.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var rsync = require("rsyncwrapper").rsync;
+
+module.exports = function(plugin, app) {
+  app.manager.RegisterTask(app, 'rsync', function(){
+    var container = app.docker.getContainer(app.config.components.data.cid);
+    container.inspect(function(err, data){
+      var dataPath = data.Volumes[plugin.volume];
+      var dest = 'rsync://root@1.3.3.7:873' + path.resolve(dataPath, plugin.dest);
+
+      rsync({
+          src: plugin.src,
+          dest: dest,
+          recursive: plugin.recursive,
+          exclude: plugin.exclude,
+          deleteAll: plugin.deleteAll
+        },
+        function (error, stdout, stderr, cmd) {
+          console.log(cmd);
+          if (err) {
+            console.log(error.message);
+          }
+        }
+      );
+
+    });
+  });
+};
