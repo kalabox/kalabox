041a26fb6e016af745a3578e62a0d84494cf1a0d
Mike Milano <coder1@gmail.com>
2014-09-23 16:59:13 -0700

    added logic to set application status based on container status

--- plugins/kalabox/index.js
+++ plugins/kalabox/index.js
@@ -1,4 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+var chalk = require('chalk');
+
+var baseDir = path.resolve(__dirname, '../../');
+var config = require(path.resolve(baseDir, 'config.json'));
+
+// Data path setup ~/.kalabox
+var homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+var dataPath = path.resolve(homePath, '.kalabox');
+var appPath = path.resolve(dataPath, 'apps');
+
 module.exports = function(plugin, app) {
+
+  app.manager.RegisterTask(app, 'list', function(){
+    var i = 1;
+    app.manager.GetApps(function(apps) {
+      console.log('');
+
+      _(apps).each(function(a) {
+        var status = '';
+        if (a.status == 'enabled') {
+          status = 'Enabled';
+          console.log(chalk.green(" " + i + ". " + a.config.title + " (" + a.appname + ")\t\t", a.url + "\t\t", status));
+        }
+        else if (a.status == 'disabled') {
+          status = 'Disabled';
+          console.log(chalk.magenta(" " + i + ". " + a.config.title + " (" + a.appname + ")\t\t", a.url + "\t\t", status));
+        }
+        else {
+          status = 'Uninstalled';
+          console.log(chalk.red(" " + i + ". " + a.config.title + " (" + a.appname + ")\t\t", a.url + "\t\t", status));
+        }
+        i++;
+      });
+      console.log('');
+    });
+  });
 
   app.manager.RegisterTask(app, 'init', function(){
     app.init();
@@ -30,6 +68,12 @@ module.exports = function(plugin, app) {
 
   app.manager.RegisterTask(app, 'build', function(){
     app.build();
+  });
+
+  app.on('post-init', function(){
+    var a = _.cloneDeep(app);
+    delete a.components;
+    fs.writeFileSync(path.resolve(app.dataPath, 'app.json'), JSON.stringify(a));
   });
 
 };
