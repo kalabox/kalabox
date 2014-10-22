5016360cf832500a085a9fe14be7322e18c772c0
Mike Milano <coder1@gmail.com>
2014-10-21 17:27:27 -0700

    added purge task

--- plugins/kalabox/index.js
+++ plugins/kalabox/index.js
@@ -4,7 +4,6 @@ var _ = require('lodash');
 var chalk = require('chalk');
 
 module.exports = function(plugin, manager) {
-
   manager.registerTask('list', function(){
     var i = 1;
     manager.getApps(function(apps) {
@@ -30,4 +29,9 @@ module.exports = function(plugin, manager) {
     });
   });
 
+  manager.registerTask('pc', function(){
+    manager.purgeContainers(function(){
+      console.log(container);
+    });
+  });
 };
