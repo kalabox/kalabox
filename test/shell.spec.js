a6df20753f19859d0628cda6494b142ff487c212
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2014-11-20 18:04:27 -0800

    Add shell.js, b2d.js, and created b2d plugin for controlling boot2docker

---
+++ test/shell.spec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var shell = require('../lib/shell.js');
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+
+describe('#shell module', function() {
+
+  describe('#exec()', function() {
+
+    it('should return the correct output.', function(done) {
+      var expected = 'elvis\n';
+      var cmd = 'echo elvis';
+      shell.exec(cmd, function(err, output) {
+        expect(output).to.equal(expected);
+        done();
+      });
+    });
+
+    it('should return an error when the cmd fails.', function(done) {
+      var cmd = 'not_a_real_program';
+      var expected = 'code: 127, msg: /bin/sh: not_a_real_program: command not found\n';
+      shell.exec(cmd, function(err, output) {
+        expect(err).to.not.equal(null);
+        expect(err.message).to.equal(expected);
+        done();
+      });
+    });
+
+  });
+
+});
