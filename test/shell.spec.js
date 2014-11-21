1f761dcdc4d53a4f4ad9c47a7bc6ba02a25f03fd
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2014-11-20 18:22:11 -0800

    Fixing a regular expression to match output of 'which' command on both Mac and Linux

--- test/shell.spec.js
+++ test/shell.spec.js
@@ -20,7 +20,7 @@ describe('#shell module', function() {
 
     it('should return an error when the cmd fails.', function(done) {
       var cmd = 'not_a_real_program';
-      var regex = /code: 127, msg: \/bin\/sh: not_a_real_program: [a-z ]*not found\n/;
+      var regex = /code: 127, msg: \/bin\/sh: [1: ]*not_a_real_program: [a-z ]*not found\n/;
       shell.exec(cmd, function(err, output) {
         expect(err).to.not.equal(null);
         expect(err.message).to.match(regex);
