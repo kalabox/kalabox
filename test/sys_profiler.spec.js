1c4df70d10c61e6cd2b0d4c424ee0ae7834c310b
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2014-12-04 13:28:16 -0800

    Wrapping OSX specific unit tests in a check, so they are only run on OSX.

--- test/sys_profiler.spec.js
+++ test/sys_profiler.spec.js
@@ -8,74 +8,95 @@ var sp = rewire('../lib/sys_profiler.js');
 
 var TIMEOUT = 45 * 1000;
 
-describe('sys_profiler.js', function() {
+var PLATFORM_OSX = 'darwin';
+var PLATFORM_WINDOWS = 'win32';
+var PLATFORM_LINUX = 'linux';
 
-  describe('#getAppData()', function() {
-
-    it('should return system profile application data.', function(done) {
-      this.timeout(TIMEOUT);
-      var expected = 'Applications:';
-      sp.getAppData(function(err, data) {
-        var match = data.match(/^(.*)\n/);
-        var result = match && match[1] ? match[1] : data.substring(0, 1024);
-        expect(err).to.equal(null);
-        expect(result).to.equal(expected);
-        done();
-      });
-    });
-
-    it('should return an applications data, when an app arg is passed.', function(done) {
-      this.timeout(TIMEOUT);
-      var expected = 'SystemUIServer';
-      sp.getAppData('SystemUIServer', function(err, data) {
-        data = data.substring(0, 1024);
-        expect(err).to.equal(null);
-        expect(data).to.equal(expected);
-        done();
-      });
-    });
-
-    it('should return null when an uninstalled app arg is passed.', function(done) {
-      this.timeout(TIMEOUT);
-      var expected = null;
-      sp.getAppData('FakeNotInstalledApp', function(err, data) {
-        if (data) {
-          data = data.substring(0, 1024);
-        }
-        expect(err).to.equal(null);
-        expect(data).to.equal(expected);
-        done();
-      });
-    });
-
+describe('#process.platform', function() {
+  it('should be an expected value.', function() {
+    var expectedPlatforms = [
+      PLATFORM_OSX,
+      PLATFORM_LINUX,
+      PLATFORM_WINDOWS
+    ];
+    var platform = process.platform;
+    expect(expectedPlatforms).to.contain(process.platform);
   });
+});
 
-  describe('#isAppInstalled()', function() {
+// Only run these unit tests if we are on OSX.
+if (process.platform === PLATFORM_OSX) {
 
-    var test = function(desc, input, expected) {
-      it(desc, function(done) {
+  describe('sys_profiler.js', function() {
+
+    describe('#getAppData()', function() {
+
+      it('should return system profile application data.', function(done) {
         this.timeout(TIMEOUT);
-        sp.isAppInstalled(input, function(err, isInstalled) {
+        var expected = 'Applications:';
+        sp.getAppData(function(err, data) {
+          var match = data.match(/^(.*)\n/);
+          var result = match && match[1] ? match[1] : data.substring(0, 1024);
           expect(err).to.equal(null);
-          expect(isInstalled).to.equal(expected);
+          expect(result).to.equal(expected);
           done();
         });
       });
-    };
 
-    test('should return TRUE when an app is installed.',
-      'SystemUIServer',
-      true
-    );
-    test('should return TRUE when an app is installed, and be case insenitive.',
-      'sYsteMuiseRver',
-      true
-    );
-    test('should return FALSE when an app is NOT installed.',
-      'FakeNotInstalledApp',
-      false
-    );
+      it('should return an applications data, when an app arg is passed.', function(done) {
+        this.timeout(TIMEOUT);
+        var expected = 'SystemUIServer';
+        sp.getAppData('SystemUIServer', function(err, data) {
+          data = data.substring(0, 1024);
+          expect(err).to.equal(null);
+          expect(data).to.equal(expected);
+          done();
+        });
+      });
+
+      it('should return null when an uninstalled app arg is passed.', function(done) {
+        this.timeout(TIMEOUT);
+        var expected = null;
+        sp.getAppData('FakeNotInstalledApp', function(err, data) {
+          if (data) {
+            data = data.substring(0, 1024);
+          }
+          expect(err).to.equal(null);
+          expect(data).to.equal(expected);
+          done();
+        });
+      });
+
+    });
+
+    describe('#isAppInstalled()', function() {
+
+      var test = function(desc, input, expected) {
+        it(desc, function(done) {
+          this.timeout(TIMEOUT);
+          sp.isAppInstalled(input, function(err, isInstalled) {
+            expect(err).to.equal(null);
+            expect(isInstalled).to.equal(expected);
+            done();
+          });
+        });
+      };
+
+      test('should return TRUE when an app is installed.',
+        'SystemUIServer',
+        true
+      );
+      test('should return TRUE when an app is installed, and be case insenitive.',
+        'sYsteMuiseRver',
+        true
+      );
+      test('should return FALSE when an app is NOT installed.',
+        'FakeNotInstalledApp',
+        false
+      );
+
+    });
 
   });
 
-});
+}
