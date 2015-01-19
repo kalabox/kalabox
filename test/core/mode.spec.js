9511adf4905b37fb5cbc987057c70073e308b907
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-01-19 14:58:46 -0800

    Added kbox.core.mode

---
+++ test/core/mode.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var rewire = require('rewire');
+var mode = rewire('../../lib/core/mode.js');
+
+describe('mode module', function() {
+
+  describe('#set()', function() {
+    
+    it('should correctly set the mode.', function() {
+      expect(mode.set('cli')).to.equal('cli');
+      expect(mode.get()).to.equal('cli');
+    });
+
+    it('should validate the mode being set.', function() {
+      var fn = function(modeToSet) {
+        return function() {
+          mode.clear();
+          mode.set(modeToSet);
+        };
+      };
+      expect(fn('cli')).to.not.throw(Error);
+      expect(fn('gui')).to.not.throw(Error);
+      expect(fn('foo')).to.throw(Error, 'Invalid mode: foo');
+      expect(fn('bar')).to.throw(Error, 'Invalid mode: bar');
+    });
+
+    it('should throw an error if it has already been set.', function() {
+      mode.clear();
+      mode.set('cli');
+      expect(function() { mode.set('gui'); }).to.throw('Mode has already been set: cli');
+    });
+
+  });
+
+});
