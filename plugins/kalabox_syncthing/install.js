1377971ab9842848564cbdd95b24848627f7bd5a
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-03-24 14:36:12 -0700

    Setup plugin kalabox_syncthing for new provision framework.

---
+++ plugins/kalabox_syncthing/install.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = function(kbox) {
+
+  kbox.install.registerStep(function(step) {
+    step.name = 'testing5';
+    step.description = step.name;
+    step.deps.push('testing4');
+    step.all = function(state, done) {
+      setTimeout(function() {
+        console.log(step.name);
+        console.log(state.testing4);
+      }, 3 * 1000);
+    };
+  });
+
+};
