58275737673a051458f67548f59be8e384d46f3a
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-03-26 21:46:22 -0700

    Added a few syncthing install steps.

--- plugins/kalabox_syncthing/install.js
+++ plugins/kalabox_syncthing/install.js
@@ -1,5 +1,32 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
 module.exports = function(kbox) {
+
+  // Is syncthing installed.
+  kbox.install.registerStep(function(step) {
+    step.name = 'is-syncthing-installed';
+    step.description = 'Check if syncthing binary is installed.';
+    step.all = function(state) {
+      state.isSyncthingInstalled = fs.existsSync(
+        path.join(state.config.sysConfRoot, 'bin', 'syncthing')
+      );
+      state.log('Syncthing installed?: ' + state.isSyncthingInstalled);
+    };
+  });
+
+  // Syncthing config exists.
+  kbox.install.registerStep(function(step) {
+    step.name = 'syncthing-config-exists';
+    step.description = 'Check if syncthing config exists.';
+    step.all = function(state) {
+      state.syncthingConfigExists = fs.existsSync(
+        path.join(state.config.sysConfRoot, 'syncthing', 'config.xml')
+      );
+      state.log('Syncthing config exists?: ' + state.syncthingConfigExists);
+    };
+  });
 
 };
