032b8408851e6720cf458b3b284851dc4e8b5551
Mike Pirog <mike@kalamuna.com>
2015-03-27 17:14:34 -0700

    #325: out with the old

--- plugins/kalabox_app/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(kbox, app) {
-
-  // Remove plugin in the future when this warning is no longer needed.
-  console.log('The kalabox_app plugin is deprecated, please remove!');
-
-};
--- plugins/kalabox_core/tasks.js
+++ plugins/kalabox_core/tasks.js
@@ -105,6 +105,99 @@ module.exports = function(kbox) {
     done();
   });
 
+  // Installs the dependencies for kalabox to run
+  kbox.core.tasks.registerTask('provision', function(done) {
+
+    var argv = kbox.core.deps.lookup('argv');
+    var config = kbox.core.deps.lookup('config');
+
+    // Logging function.
+    var log = function(msg) {
+      if (msg) {
+        console.log('#### ' + msg + ' ####');
+        //console.log(chalk.green('#### ' + msg + ' ####'));
+      } else {
+        console.log('');
+      }
+    };
+
+    // Test provision, just print out step info.
+    if (argv.t) {
+      var steps = kbox.install.getSteps();
+      // Output each step to console.
+      steps.forEach(function(step) {
+        console.log(step);
+      });
+      // Return.
+      return done();
+    }
+
+    // State to inject into install.
+    var state = {
+      adminCommands: [],
+      config: config,
+      downloads: [],
+      log: console.log,
+      status: {
+        ok: chalk.green('OK'),
+        notOk: chalk.red('NOT OK')
+      }
+    };
+
+    // Keep track of which step we are on.
+    var stepCount = 1;
+
+    // Total number of steps.
+    var totalSteps = kbox.install.stepCount();
+
+    // Get the current time in milliseconds.
+    var getTime = function() {
+      return Date.now();
+    };
+
+    // Time the install started.
+    var startTime = getTime();
+
+    // Time the current step started.
+    var stepStartTime = startTime;
+
+    // Runs right before step.
+    kbox.install.events.on('pre-step', function(step) {
+      var stepNumberInfo = [stepCount, totalSteps].join(':');
+      var stepInfo = 'Starting ' + step.name;
+
+      log('[' + stepNumberInfo + '] ' + stepInfo);
+      log('description => ' + step.description);
+      log('dependencies => ' + step.deps.join(', '));
+
+      stepCount += 1;
+    });
+
+    // Runs right after step.
+    kbox.install.events.on('post-step', function(step) {
+      var now = getTime();
+      var duration = now - stepStartTime;
+      stepStartTime = now;
+
+      log('Finished ' + step.name + ' (' + duration + ')');
+      log();
+    });
+
+    // Error.
+    kbox.install.events.on('error', function(err) {
+      done(err);
+    });
+
+    // Install is done.
+    kbox.install.events.on('end', function(state) {
+      done();
+    });
+
+    // Run the installer.
+    kbox.install.run(state);
+
+  });
+
   /*
    * TASKS THAT **DO** NEED THE APP OBJECT.
    */
