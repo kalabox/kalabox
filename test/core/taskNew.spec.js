d5582a0d45ef79ec1c2bf97064e3f7c0e092c472
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-04-02 14:17:39 -0700

    Much more work done on new cli task framework and basic unit tests added.

---
+++ test/core/taskNew.spec.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var m = require('../../lib/core/taskNew.js');
+
+describe.only('CLI Task Module', function() {
+
+  describe('#registerTask()', function() {
+
+    it('Should add tasks to the task tree in the correct way.', function() {
+
+      m.registerTask(function(task) {
+        task.path = ['a', 'b', 'd'];
+        task.description = task.path.join('');
+        task.func = function() {
+          return task.description;
+        };
+      });
+
+      m.registerTask(function(task) {
+        task.path = ['a', 'b', 'c'];
+        task.description = task.path.join('');
+        task.func = function() {
+          return task.description;
+        };
+      });
+
+      m.registerTask(function(task) {
+        task.path = ['a', 'x'];
+        task.description = task.path.join('');
+        task.func = function() {
+          return task.description;
+        };
+      });
+
+      /*
+
+      root
+        a
+          b
+            c
+            d
+          x
+
+      */
+
+      var root = m.getTaskTree();
+      expect(root.__isBranch).to.equal(true);
+      expect(root.name).to.equal('root');
+      expect(root.children.length).to.equal(1);
+      
+      var a = root.children[0];
+      expect(a.__isBranch).to.equal(true);
+      expect(a.name).to.equal('a');
+      expect(a.children.length).to.equal(2);
+
+      var b = a.children[0];
+      expect(b.__isBranch).to.equal(true);
+      expect(b.name).to.equal('b');
+      expect(b.children.length).to.equal(2);
+
+      var c = b.children[0];
+      expect(c.__isTask).to.equal(true);
+      expect(c.path).to.deep.equal(['c']);
+      var d = b.children[1];
+      expect(d.__isTask).to.equal(true);
+      expect(d.path).to.deep.equal(['d']);
+
+      var x = a.children[1];
+      expect(x.__isTask).to.equal(true);
+      expect(x.path).to.deep.equal(['x'])
+
+    });
+
+  });
+
+});
