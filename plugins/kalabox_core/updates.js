a163b3e1708b3217dea7075a82e67875e6271739
Mike Pirog <mike@kalamuna.com>
2015-04-03 13:45:43 -0700

    #371: Keep it safe, although not secret

--- plugins/kalabox_core/updates.js
+++ plugins/kalabox_core/updates.js
@@ -6,18 +6,18 @@
 
 var prompt = require('prompt');
 
+var PROVIDER_ATTEMPTS = 3;
+
 module.exports = function(kbox) {
 
   var argv = kbox.core.deps.lookup('argv');
+  var helpers = kbox.util.helpers;
 
   // Authorize the update process
   kbox.update.registerStep(function(step) {
-    step.name = 'update-auth';
+    step.name = 'kbox-auth';
     step.description = 'Authorizing update subroutines...';
     step.all = function(state, done) {
-      //
-      // Setting these properties customizes the prompt.
-      //
       prompt.override = argv;
       prompt.start();
       prompt.get({
@@ -44,15 +44,16 @@ module.exports = function(kbox) {
 
   // Authorize the update process
   kbox.update.registerStep(function(step) {
-    step.name = 'update-backends';
-    step.deps = ['update-auth'];
-    step.description = 'Updating your backend codes.';
+    step.name = 'kbox-backends';
+    step.deps = ['kbox-update'];
+    step.description = 'Updating your Kalabox backends.';
     step.all = function(state, done) {
       kbox.util.npm.updateBackends(function(err) {
         if (err) {
           done(err);
         }
         else {
+          state.log('Updated kalabox backends!');
           done();
         }
       });
@@ -61,19 +62,103 @@ module.exports = function(kbox) {
 
   // Authorize the update process
   kbox.update.registerStep(function(step) {
-    step.name = 'update-backends';
-    step.deps = ['update-auth'];
-    step.description = 'Updating your backend codes.';
+    step.name = 'kbox-update';
+    step.deps = ['kbox-auth'];
+    step.description = 'Updating your Kalabox codes.';
     step.all = function(state, done) {
-      kbox.util.npm.updateBackends(function(err) {
+      kbox.util.npm.updateKalabox(function(err) {
         if (err) {
           done(err);
         }
         else {
+          state.log('Updated kalabox dependencies!');
           done();
         }
       });
     };
   });
 
+  // Shutdown running apps and containers
+  // this should go in kalabox-engine-docker
+  kbox.update.registerStep(function(step) {
+    step.name = 'engine-prepare';
+    step.deps = ['kbox-backends'];
+    step.description = 'Preparing engine for updates.';
+    step.all = function(state, done) {
+      kbox.engine.up(PROVIDER_ATTEMPTS, function(err) {
+        if (err) {
+           done(err);
+         }
+         else {
+          kbox.engine.list(function(err, containers) {
+            if (err) {
+              done(err);
+            }
+            else {
+              helpers.mapAsync(
+                containers,
+                function(container, done) {
+                  kbox.engine.info(container.id, function(err, info) {
+                    if (err) {
+                      done(err);
+                    } else {
+                      if (!info.running) {
+                        done();
+                      }
+                      else {
+                        if (info.app !== null) {
+                          kbox.app.get(info.app, function(err, app) {
+                            if (err) {
+                              done(err);
+                            }
+                            else {
+                              kbox.app.stop(app, function(errs) {
+                                if (errs) {
+                                  done(errs);
+                                }
+                                else {
+                                  state.log('Stopped ' + info.name);
+                                  done();
+                                }
+                              });
+                            }
+                          });
+                        }
+                        else {
+                          kbox.engine.stop(info.id, function(err) {
+                            if (err) {
+                              done(err);
+                            }
+                            else {
+                              kbox.engine.remove(info.id, function(err) {
+                                if (err) {
+                                  done(err);
+                                }
+                                else {
+                                  state.log('Removed ' + info.name);
+                                  done();
+                                }
+                              });
+                            }
+                          });
+                        }
+                      }
+                    }
+                  });
+                },
+                function(errs) {
+                  if (err) {
+                    done(err);
+                  }
+                  else {
+                    kbox.engine.down(PROVIDER_ATTEMPTS, done);
+                  }
+                }
+              );
+            }
+          });
+         }
+      });
+    };
+  });
 };
