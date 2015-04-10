540bbfe53647f573174085133c48f7fd6a858f77
Mike Pirog <mike@kalamuna.com>
2015-04-10 14:14:05 -0700

    Need these tasks on win and nix

--- plugins/kalabox-core/steps/common.js
+++ plugins/kalabox-core/steps/common.js
@@ -60,6 +60,14 @@ module.exports = function(kbox, framework) {
         done(err);
       });
     };
+    step.all.linux = function(state, done) {
+      // @todo
+      done();
+    };
+    step.all.win32 = function(state, done) {
+      // @todo
+      done();
+    };
   });
 
   // Internet.
