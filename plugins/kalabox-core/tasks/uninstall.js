f7a198de6c738a98144a79cef8e54647a9608984
Mike Pirog <mike@kalamuna.com>
2015-04-16 09:26:00 -0700

    #398: Remove code directory on uninstall

--- plugins/kalabox-core/tasks/uninstall.js
+++ plugins/kalabox-core/tasks/uninstall.js
@@ -1,12 +1,17 @@
 'use strict';
 
+var rmdir = require('rimraf');
+
 /**
  * This contains all the core commands that kalabox can run on every machine
  */
 
 module.exports = function(kbox) {
 
+  var events = kbox.core.events;
+
   kbox.whenApp(function(app) {
+
     kbox.tasks.add(function(task) {
       task.path = [app.name, 'uninstall'];
       task.description = 'Uninstall an installed kbox application';
@@ -14,6 +19,19 @@ module.exports = function(kbox) {
         kbox.app.uninstall(app, done);
       };
     });
+
+    events.on('post-uninstall', function(app, done) {
+      console.log('Removing the codez.');
+      rmdir(app.config.codeRoot, function(error) {
+        if (err) {
+          done(err);
+        }
+        else {
+          done();
+        }
+      });
+    });
+
   });
 
 };
