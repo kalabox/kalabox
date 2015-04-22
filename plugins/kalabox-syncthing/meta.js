9f1dffe834dbc90dd1cffed3876a885cd9da5b68
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-04-22 13:53:17 -0700

    Updating syncthing download URLS.

--- plugins/kalabox-syncthing/meta.js
+++ plugins/kalabox-syncthing/meta.js
@@ -3,11 +3,11 @@
 module.exports = {
   SYNCTHING_DOWNLOAD_URL: {
     darwin: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.10.21/syncthing-macosx-amd64-v0.10.21.tar.gz',
+      'v0.11.0/syncthing-macosx-amd64-v0.11.0.tar.gz',
     win32: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.10.21/syncthing-windows-amd64-v0.10.21.zip',
+      'v0.11.0/syncthing-windows-amd64-v0.11.0.zip',
     linux: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.10.21/syncthing-linux-amd64-v0.10.21.tar.gz'
+      'v0.11.0/syncthing-linux-amd64-v0.11.0.tar.gz'
   },
   SYNCTHING_CONFIG_URL: 'https://raw.githubusercontent.com/kalabox/' +
     'kalabox-dockerfiles/master/syncthing/config.xml'
