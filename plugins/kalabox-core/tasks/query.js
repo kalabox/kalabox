6bfda8a96106ff5a9bd3cd9c9eac6ec71dd1ebac
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-06-12 09:59:33 -0700

    Refactored 'kbox query' command for better functional testing.

--- plugins/kalabox-core/tasks/query.js
+++ plugins/kalabox-core/tasks/query.js
@@ -9,16 +9,17 @@ module.exports = function(kbox) {
     task.description = 'Run a command against a container.';
     task.kind = 'argv*';
     task.func = function(done) {
-      var container = _.head(this.payload);
+      var target = _.head(this.payload);
       var cmd = _.tail(this.payload);
       var stdout = process.stdout;
       var stderr = process.stderr;
-      kbox.engine.query(container, cmd, stdout, stderr, function(err) {
-        if (err) {
-          return done(err);
-        }
-        process.stdout.on('end', done);
-      });
+      kbox.engine.query(target, cmd)
+      .then(function(res) {
+        res.stdout.pipe(process.stdout);
+        res.stderr.pipe(process.stderr);
+        return res.wait();
+      })
+      .nodeify(done);
     };
   });
 
