dbcc89f0747c990a18f145d89cc08f4c429dcb76
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-06-22 13:10:03 -0700

    Upgrading to syncthing v0.11.10.

--- plugins/kalabox-syncthing/meta.js
+++ plugins/kalabox-syncthing/meta.js
@@ -3,11 +3,11 @@
 module.exports = {
   SYNCTHING_DOWNLOAD_URL: {
     darwin: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.11.6/syncthing-macosx-amd64-v0.11.6.tar.gz',
+      'v0.11.10/syncthing-macosx-amd64-v0.11.10.tar.gz',
     win32: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.11.6/syncthing-windows-amd64-v0.11.6.zip',
+      'v0.11.10/syncthing-windows-amd64-v0.11.10101010101010101010.zip',
     linux: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.11.6/syncthing-linux-amd64-v0.11.6.tar.gz'
+      'v0.11.10/syncthing-linux-amd64-v0.11.10.tar.gz'
   },
   SYNCTHING_CONFIG_URL: 'https://raw.githubusercontent.com/kalabox/' +
     'kalabox/master/dockerfiles/syncthing/config.xml'
