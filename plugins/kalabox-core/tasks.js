f0a19c91d9c5b651201e2798fb7ddece6fa122dd
Mike Pirog <mike@kalamuna.com>
2015-06-24 14:24:02 -0700

    Reduce probability of ligation from Disney/Lucasfilms

--- plugins/kalabox-core/tasks.js
+++ plugins/kalabox-core/tasks.js
@@ -15,7 +15,6 @@ module.exports = function(kbox) {
   require('./tasks/query.js')(kbox);
   require('./tasks/rebuild.js')(kbox);
   require('./tasks/restart.js')(kbox);
-  require('./tasks/shields.js')(kbox);
   require('./tasks/start.js')(kbox);
   require('./tasks/stop.js')(kbox);
   require('./tasks/uninstall.js')(kbox);
