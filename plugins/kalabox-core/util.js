03c2034e93981c05eb3eeac926b4520d594622e3
Mike Pirog <mike@kalamuna.com>
2015-08-28 19:27:51 -0700

    #218: Move dev tasks into new dev plugin

--- plugins/kalabox-core/util.js
+++ plugins/kalabox-core/util.js
@@ -12,27 +12,6 @@ module.exports = function(kbox) {
 
   var helpers = kbox.util.helpers;
 
-  /*
-   * Return true if container matches query.
-   */
-  var isContainerMatch = function(container, appName, query) {
-
-    // Match query against container's id.
-    var isContainerId = query === container.id;
-
-    // Match query against container's name.
-    var isContainerName = query === container.name;
-
-    // Match query against component's name.
-    var isComponentName = (function() {
-      var o = kbox.util.docker.containerName.parse(container.name);
-      return o.app === appName && o.name === query;
-    })();
-
-    return isContainerId || isContainerName || isComponentName;
-
-  };
-
   var outputContainers = function(app, done) {
     if (typeof app === 'function' && !done) {
       done = app;
@@ -310,7 +289,6 @@ module.exports = function(kbox) {
   return {
     outputContainers: outputContainers,
     createFrameworkFunc: createFrameworkFunc,
-    isContainerMatch: isContainerMatch,
     runAdminCmds: runAdminCmds,
     prepareImages: prepareImages
   };
