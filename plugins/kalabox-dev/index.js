655180d5258033b8c668b84a909c1d6a20942110
Mike Pirog <mike@kalamuna.com>
2015-08-28 19:44:40 -0700

    #218: whoops cs

--- plugins/kalabox-dev/index.js
+++ plugins/kalabox-dev/index.js
@@ -3,7 +3,7 @@
 module.exports = function(kbox) {
   // Load CLI tasks for this plugin.
   var profile = kbox.core.deps.get('globalConfig').profile;
-  var envDev = process.env['KALABOX_DEV'];
+  var envDev = process.env.KALABOX_DEV;
   if (profile === 'dev' || envDev === 'true') {
     require('./tasks.js')(kbox);
   }
