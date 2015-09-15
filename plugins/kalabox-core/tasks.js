918b7e2bec961f9ac757596203a47dcccbbc0c8d
Mike Pirog <mike@kalamuna.com>
2015-09-15 11:26:03 -0700

    #447: Add update command and allow installer run to accept state options

--- plugins/kalabox-core/tasks.js
+++ plugins/kalabox-core/tasks.js
@@ -9,11 +9,12 @@ module.exports = function(kbox) {
   require('./tasks/apps.js')(kbox);
   require('./tasks/config.js')(kbox);
   require('./tasks/containers.js')(kbox);
+  require('./tasks/destroy.js')(kbox);
   require('./tasks/rebuild.js')(kbox);
   require('./tasks/restart.js')(kbox);
   require('./tasks/start.js')(kbox);
   require('./tasks/stop.js')(kbox);
-  require('./tasks/destroy.js')(kbox);
+  require('./tasks/update.js')(kbox);
   require('./tasks/version.js')(kbox);
 
 };
