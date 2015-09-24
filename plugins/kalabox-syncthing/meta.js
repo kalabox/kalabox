726a28159d17500b1a7bde0e3b25beaf0045dab3
Mike Pirog <mike@kalamuna.com>
2015-09-23 18:56:01 -0700

    #509: Upgrade syncthing to latest 0.11.x

--- plugins/kalabox-syncthing/meta.js
+++ plugins/kalabox-syncthing/meta.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = {
-  SYNCTHING_BINARY: '0.11.22',
-  SYNCTHING_IMAGE: '0.11.22',
+  SYNCTHING_BINARY: '0.11.25',
+  SYNCTHING_IMAGE: '0.11.25',
   SYNCTHING_CONFIG: '0.10.0',
   SYNCTHING_DOWNLOAD_URL: {
     darwin: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.11.22/syncthing-macosx-amd64-v0.11.22.tar.gz',
+      'v0.11.25/syncthing-macosx-amd64-v0.11.25.tar.gz',
     win32: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.11.22/syncthing-windows-amd64-v0.11.22.zip',
+      'v0.11.25/syncthing-windows-amd64-v0.11.25.zip',
     linux: 'https://github.com/syncthing/syncthing/releases/download/' +
-      'v0.11.22/syncthing-linux-amd64-v0.11.22.tar.gz'
+      'v0.11.25/syncthing-linux-amd64-v0.11.25.tar.gz'
   },
   SYNCTHING_CONFIG_URL: 'https://raw.githubusercontent.com/kalabox/' +
     'kalabox/master/dockerfiles/syncthing/config.xml'
