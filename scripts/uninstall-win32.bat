4d199f5309145a012309926ad3e6c8a67ca88101
Mike Pirog <mike@kalamuna.com>
2015-09-28 18:06:18 -0700

    #524: Chaos on DOZE

--- scripts/uninstall-win32.bat
+++ scripts/uninstall-win32.bat
@@ -38,7 +38,7 @@ IF %id% == 1 (
     ECHO Removing Kalabox directories
     RMDIR %HOMEPATH%\.kalabox /S /Q
     RMDIR "%HOMEPATH%\VirtualBox VMs\Kalabox2" /S /Q
-    DEL %HOMEPATH%\.ssh\boot2docker.kalabox.id_rsa.pub \ /S /Q
+    DEL %HOMEPATH%\.ssh\boot2docker.kalabox.id_rsa.pub /S /Q
     DEL %HOMEPATH%\.ssh\boot2docker.kalabox.id_rsa /S /Q
     ECHO REMOVED!
     ECHO.
