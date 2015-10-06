fd1674579d2a76d09259a77845bfe608723b34ca
Mike Pirog <mike@kalamuna.com>
2015-10-06 11:41:16 -0700

    #362: Add a clear confirm step for kbox destroy

--- plugins/kalabox-core/tasks/destroy.js
+++ plugins/kalabox-core/tasks/destroy.js
@@ -6,15 +6,53 @@
 
 module.exports = function(kbox) {
 
+  // Npm modules
+  var inquirer = require('inquirer');
+  var chalk = require('chalk');
+
   kbox.whenAppRegistered(function(app) {
 
     kbox.tasks.add(function(task) {
       task.path = [app.name, 'destroy'];
       task.category = 'appAction';
+      task.options.push({
+        name: 'yes',
+        alias: 'y',
+        kind: 'boolean',
+        description: 'Automatically answer affirmitive'
+      });
       task.description = 'Completely destroys and removes an app.';
       task.func = function(done) {
 
-        kbox.app.destroy(app, done);
+        // Display ominous warning
+        console.log(kbox.art.destroyWarn());
+
+        // Needs to prompt?
+        var confirmPrompt = !this.options.yes;
+
+        // Set up our confirmation question if needed
+        var confirm = {
+          type: 'confirm',
+          name: 'doit',
+          message: 'So, are you still prepared to DESTROY?',
+          when: function(answers) {
+            return confirmPrompt;
+          }
+        };
+
+        // Destroyer of worlds
+        inquirer.prompt([confirm], function(answers) {
+          // Destroy if confirmed
+          if (answers.doit) {
+            kbox.app.destroy(app, done);
+          }
+          // Print and exit
+          else {
+            console.log(chalk.green('WHEW! IMMINENT DESTRUCTION AVERTED!'));
+            done();
+          }
+
+        });
 
       };
     });
