bf4a83c8e158e7389cec92865b378844a31b1eed
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-11-17 18:36:21 -0800

    Fixing internet check unit test.

--- test/internet.spec.js
+++ test/internet.spec.js
@@ -13,13 +13,8 @@ describe('internet.js', function() {
 
     it('should return true when a site is reachable.', function(done) {
       this.timeout(10 * 1000);
-      return internet.check('www.google.com')
-
-      .then(function(connected) {
-        expect(connected).to.equal(true);
-      })
-
-      .nodeify(done);
+      return internet.check()
+      .then(done);
     });
 
   });
