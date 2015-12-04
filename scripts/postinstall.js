7d05f438990ae793328e219415d7ce8a39a807cc
Mike Pirog <mike@kalamuna.com>
2015-12-03 23:00:12 -0800

    #757: Update devMode to use custom dev conf file

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,13 +5,7 @@
 
 'use strict';
 
-// Native mods
-var path = require('path');
-var url = require('url');
-
 // Npm mods
-var _ = require('lodash');
-var fs = require('fs-extra');
 var Promise = require('bluebird');
 
 // Some kbox dependencies
@@ -20,128 +14,6 @@ var config = require('./../lib/core/config.js');
 
 // Grab our global config
 var globalConfig = config.getGlobalConfig();
-
-/*
- * We can't rely on Kalabox stuff in this file so we need to do this
- * to get the users home dir
- */
-var getHomeDir = function() {
-
-  // Check the env for the home path
-  var platformIsWindows = process.platform === 'win32';
-  var envKey = platformIsWindows ? 'USERPROFILE' : 'HOME';
-
-  // Homeward bound
-  return process.env[envKey];
-
-};
-
-/*
- * We can't rely on inited Kalabox stuff in this file since we
- * are going to use it in postinstall so we need to do this
- * to get dev mod
- */
-var getDevMode = function() {
-
-  // Set for usage later
-  var devMode;
-
-  // If the environment is a no go try to load from custom kalabox.json
-  if (process.env.KALABOX_DEV === undefined) {
-
-    // Construct kbox.json path
-    var kboxJsonFile = path.join(getHomeDir(), '.kalabox', 'kalabox.json');
-
-    // Check it kbox json exists
-    if (fs.existsSync(kboxJsonFile)) {
-
-      // Load it
-      var kboxJson = require(kboxJsonFile);
-
-      // Use its dev mode if its set
-      devMode = (kboxJson.devMode) ? kboxJson.devMode : false;
-
-    }
-  }
-
-  // Use the envvar if its set
-  else {
-    // Set it
-    devMode = (process.env.KALABOX_DEV) ? process.env.KALABOX_DEV : false;
-  }
-
-  // Return something
-  return devMode;
-
-};
-
-/*
- * Get the project tag/branch we should be pulling from
- */
-var getProjectVersion = function(project) {
-
-  // If we are in dev mode then grab
-  if (getDevMode() === true || getDevMode() === 'true') {
-    var vParts = globalConfig.version.split('.');
-    return 'v' + [vParts[0], vParts[1]].join('.');
-  }
-  // If not we just return the version we already have
-  else {
-    var parts = project.split('@');
-    return parts[1];
-  }
-
-};
-
-/**
- * Returns a string with the url of the master branch
- * tarball. The package must be on github and have a repository
- * field in its pacakge.json. It must also have a master branch that is
- * considered the development branch on github.
- */
-var getTarball = function(pkg, version) {
-
-  // Build our tarball URL
-  // https://github.com/kalabox/kalabox-plugin-dbenv/tarball/master
-  var tarUrl = {
-    protocol: 'https:',
-    host: 'github.com',
-    pathname: ['kalabox', pkg, 'tarball', version].join('/')
-  };
-
-  // Return the formatted tar URL
-  return url.format(tarUrl);
-
-};
-
-/*
- * Replaces the version part of a npm pkg@version string with
- * the master branch tarball from github if that package is a kalabox
- * plugin
- *
- * If you are using your own external app or plugin it needs to live on github
- * and have a master branch or this is not going to work.
- *
- */
-var pkgToDev = function(pkg, version) {
-
-  // Split our package so we can reassemble later
-  var parts = pkg.split('@');
-
-  // Grab the dev tarball if ths is a kalabox plugin
-  if (_.includes(pkg, 'kalabox-')) {
-
-    // Get the tarball location
-    var tar = getTarball(parts[0], version);
-    return [parts[0], tar].join('@');
-
-  }
-  // Otherwise just return what we have
-  else {
-    return pkg;
-  }
-
-};
 
 var installPackages = function(pkgs) {
 
@@ -156,14 +28,6 @@ var installPackages = function(pkgs) {
 
   // Install each pkg after we check whether we should grab dev mode or not
   .each(function(pkg) {
-
-    // Figure out which version we should be trying to install
-    var version = getProjectVersion(pkg);
-
-    // If we need the dev branch do the transform
-    if (version.charAt(0) === 'v') {
-      pkg = pkgToDev(pkg, version);
-    }
 
     // Debug log the package
     console.log(pkg);
