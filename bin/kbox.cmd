649a94b62ed1ad6561683e359da56b5d9ce2488e
Mike Pirog <mike@kalamuna.com>
2015-12-04 16:17:03 -0800

    #757: Update windows cli bin for TED

--- bin/kbox.cmd
+++ bin/kbox.cmd
@@ -1,6 +1,16 @@
-:: Created by npm, please don't edit manually.
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe" "%~dp0.\node_modules\kalabox\bin\kbox.js" %*
+@ECHO OFF
+
+SETLOCAL
+
+SET "NODE_EXE=%~dp0\node.exe"
+IF NOT EXIST "%NODE_EXE%" (
+  SET "NODE_EXE=node"
 ) ELSE (
-  node "%~dp0.\node_modules\kalabox\bin\kbox.js" %*
+  SET "KBOX_CLI_JS=%~dp0.\node_modules\kalabox\bin\kbox.js"
 )
+
+IF NOT EXIST "%KBOX_CLI_JS%" (
+  SET "KBOX_CLI_JS=%~dp0\kbox.js"
+)
+
+"%NODE_EXE%" "%KBOX_CLI_JS%" %*
