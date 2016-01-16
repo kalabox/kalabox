6153574cbfe7468b6ff41593fc02258be8d80616
Mike Pirog <mike@kalamuna.com>
2016-01-15 17:14:44 -0800

    This is what we want

--- plugins/kalabox-engine-docker/provider/docker/lib/bin.js
+++ plugins/kalabox-engine-docker/provider/docker/lib/bin.js
@@ -82,7 +82,7 @@ module.exports = function(kbox) {
     if (opts && opts.mode) {
 
       // Stdio per mode
-      var collect = {stdio: ['pipe', 'pipe', 'pipe']};
+      var collect = {stdio: ['inherit', 'pipe', 'inherit']};
       var attach = {stdio: ['inherit', 'inherit', 'inherit']};
       var stdio = (opts.mode === 'attach') ? attach : collect;
 
