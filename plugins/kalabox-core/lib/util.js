8de28d3d9e22cc48c0c8c787ebca4e9bec55ee62
Mike Pirog <mike@kalamuna.com>
2016-02-04 15:11:12 -0800

    #919: Download kalabox binary in GUI mode and set in path

--- plugins/kalabox-core/lib/util.js
+++ plugins/kalabox-core/lib/util.js
@@ -4,9 +4,20 @@
  * This contains all the core commands that kalabox can run on every machine
  */
 
-var _ = require('lodash');
-
 module.exports = function(kbox) {
+
+  // Node modules
+  var path = require('path');
+
+  // NPM modules
+  var _ = require('lodash');
+  var fs = require('fs-extra');
+
+  // Kalabox modules
+  var install = kbox.install;
+
+  // Get and load the install config
+  var config = kbox.util.yaml.toJson(path.join(__dirname, 'config.yml'));
 
   /*
    * Return a list of all the names of apps kbox knows about.
@@ -112,11 +123,73 @@ module.exports = function(kbox) {
 
   };
 
+  /*
+   * Helper function to assess whether we need a new B2D
+   */
+  var needsKboxBinary = function() {
+    return install.getProUp('KBOX_BIN_VERSION', config.kalabox.version);
+  };
+
+  /*
+   * Helper function to assess whether we need a new B2D
+   */
+  var kboxBin2Path = function() {
+
+    // Get the sysbin root
+    var sysBinRoot = path.join(kbox.core.deps.get('config').sysConfRoot, 'bin');
+
+    // Return on windows
+    if (process.platform === 'win32') {
+      return ['setx', 'PATH', '"%PATH%;' + sysBinRoot + '"', '/M'].join(' ');
+    }
+
+    // Return on posix
+    // @todo: we might need to verify /usr/local/bin/ exists
+    else {
+
+      // Source and target file
+      var kboxBin = (process.platform === 'win32') ? 'kbox.exe' : 'kbox';
+      var source = path.join(sysBinRoot, kboxBin);
+      var target = path.join('/usr/local/bin', kboxBin);
+      return ['ln', '-sf', source, target].join(' ');
+    }
+  };
+
+  /*
+   * Helper function to install the syncthing binary
+   */
+  var installKboxBinary = function(state) {
+
+    // Source path
+    var downloadDir = kbox.util.disk.getTempDir();
+    var srcFile = path.basename(config.kalabox.pkg[process.platform]);
+
+    // Destination path
+    var sysConfRoot = kbox.core.deps.get('config').sysConfRoot;
+    var kboxBinDest = path.join(sysConfRoot, 'bin');
+    var destFile = 'kbox';
+    var destExt = (process.platform === 'win32') ? '.exe' : '';
+
+    // Move the kbox bin over to the kbox bin location
+    var source = path.join(downloadDir, path.basename(srcFile));
+    var dest = path.join(kboxBinDest, destFile + destExt);
+    state.log.debug('INSTALLING ' + source + ' FROM => ' + downloadDir);
+    fs.copySync(source, dest, {clobber: true});
+    state.log.debug('INSTALLED ' + dest + ' TO => ' + kboxBinDest);
+
+    // Make executable
+    fs.chmodSync(dest, '0755');
+
+  };
+
   return {
     getAppStats: getAppStats,
     getAppContainers: getAppContainers,
     getAppNames: getAppNames,
-    outputContainers: outputContainers
+    kboxBin2Path: kboxBin2Path,
+    outputContainers: outputContainers,
+    needsKboxBinary: needsKboxBinary,
+    installKboxBinary: installKboxBinary
   };
 
 };
