192aea3d02a1694c42a5673640e3dac109320ccc
Mike Pirog <mike@kalamuna.com>
2016-03-03 10:47:43 -0800

    #1074: basic wordpress support

--- plugins/kalabox-php/index.js
+++ plugins/kalabox-php/index.js
@@ -52,7 +52,8 @@ module.exports = function(kbox) {
   // List of viable frameworks
   var frameworks = [
     'drupal7',
-    'drupal8'
+    'drupal8',
+    'wordpress'
   ];
 
   // Load our events and create modules
--- plugins/kalabox-php/lib/events.js
+++ plugins/kalabox-php/lib/events.js
@@ -3,6 +3,9 @@
 
 module.exports = function(kbox) {
 
+  // NPM modules
+  var _ = require('lodash');
+
   // Load some boxmods
   var events = kbox.core.events.context('29b1da3b-e0d0-49e3-a343-ea558a21c6e2');
 
@@ -12,11 +15,25 @@ module.exports = function(kbox) {
    */
   events.on('pre-create-configure', function(config) {
 
-    // Only run if this is a drupal app
-    if (config.type === 'drupal') {
-      // Set the drupal version into the config
-      var drupalVersion = kbox.core.deps.get('argv').payload.pop();
-      config.pluginconfig.drupal.version = drupalVersion.replace('drupal', '');
+    // Only run if this is a php app
+    if (config.type === 'php') {
+
+      // Get the created app type
+      var created = kbox.core.deps.get('argv').payload.pop();
+
+      // Get the framework and version in various ways
+      // For drupal
+      if (_.includes(created, 'drupal')) {
+        config.pluginconfig.php.framework = 'drupal';
+        config.pluginconfig.php.version = created.replace('drupal', '');
+      }
+
+      // For wordpress
+      else if (created === 'wordpress') {
+        config.pluginconfig.php.framework = 'wordpress';
+        config.pluginconfig.php.version = '4';
+      }
+
     }
 
   });
