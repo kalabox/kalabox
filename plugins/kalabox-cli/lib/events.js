49e577c23eb099bebb32af444079a04b35cf3f8a
Mike Pirog <mike@kalamuna.com>
2016-03-03 11:04:19 -0800

    #1074: Add events to CLI plugin to add additional compose and cli files

--- plugins/kalabox-cli/lib/events.js
+++ plugins/kalabox-cli/lib/events.js
@@ -4,6 +4,7 @@ module.exports = function(kbox) {
 
   // Node
   var path = require('path');
+  var fs = require('fs');
 
   // NPM Modules
   var _ = require('lodash');
@@ -21,14 +22,33 @@ module.exports = function(kbox) {
      */
     if (_.get(app.config.pluginconfig, 'cli') === 'on') {
 
-      // Uniquely add our cli tools
-      var cliCompose = path.join(app.root, 'kalabox-cli.yml');
-      app.composeCore.push(cliCompose);
-      app.composeCore = _.uniq(app.composeCore);
-
       // Set a default value of null for this here so we dont
       // mess up other stuff
       env.setEnv('KALABOX_CLI_WORKING_DIR', '');
+
+      // Grab the default compose file
+      var composeFiles = [path.join(app.root, 'kalabox-cli.yml')];
+
+      // Allow other things to add task files
+      return kbox.core.events.emit('cli-add-composefiles', composeFiles)
+
+      // Then load all the composefiles up
+      .then(function() {
+
+        // Check for our tasks and then generate tasks if the file
+        // exists
+        _.forEach(composeFiles, function(composeFile) {
+
+          if (fs.existsSync(composeFile)) {
+            app.composeCore.push(composeFile);
+          }
+
+        });
+
+        // Make sure we are always unique
+        app.composeCore = _.uniq(app.composeCore);
+
+      });
 
     }
 
