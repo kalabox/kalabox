f514423d387df518a178083b05e3204cf0f41c2b
Mike Pirog <mike@kalamuna.com>
2016-03-18 15:58:48 -0700

    #1099: Get travis to build our packages

---
+++ ci/travis-ci.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+COMMAND=$1
+EXIT_VALUE=0
+
+##
+# SCRIPT COMMANDS
+##
+
+# before-install
+#
+# Do some stuff before npm install
+#
+before-install() {
+
+  # Gather intel
+  echo "TRAVIS_TAG: ${TRAVIS_TAG}"
+  echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+  echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+  echo "TRAVIS_REPO_SLUG: ${TRAVIS_REPO_SLUG}"
+  echo "TRAVIS_BUILD_DIR: ${TRAVIS_BUILD_DIR}"
+  echo "TRAVIS_OS_NAME: ${TRAVIS_OS_NAME}"
+  echo "PATH: ${PATH}"
+
+}
+
+# install
+#
+# Install our project
+#
+install() {
+  make
+}
+
+
+# before-script
+#
+# Run before tests
+#
+before-script() {
+  echo
+}
+
+# script
+#
+# Run the tests.
+#
+script() {
+  echo
+}
+
+# after-script
+#
+# Clean up after the tests.
+#
+after-script() {
+  echo
+}
+
+# after-success
+#
+# Clean up after the tests.
+#
+after-success() {
+  echo
+}
+
+# before-deploy
+#
+# Do stuff before deploy
+#
+before-deploy() {
+  echo
+}
+
+# after-deploy
+#
+# Do stuff after deploy
+#
+after-deploy() {
+  echo
+}
+
+##
+# UTILITY FUNCTIONS:
+##
+
+# Sets the exit level to error.
+set_error() {
+  EXIT_VALUE=1
+  echo "$@"
+}
+
+# Runs a command and sets an error if it fails.
+run_command() {
+  set -xv
+  if ! $@; then
+    set_error
+  fi
+  set +xv
+}
+
+##
+# SCRIPT MAIN:
+##
+
+# Capture all errors and set our overall exit value.
+trap 'set_error' ERR
+
+# We want to always start from the same directory:
+cd $TRAVIS_BUILD_DIR
+
+case $COMMAND in
+  before-install)
+    run_command before-install
+    ;;
+
+  install)
+    run_command install
+    ;;
+
+  before-script)
+    run_command before-script
+    ;;
+
+  script)
+    run_command script
+    ;;
+
+  after-script)
+    run_command after-script
+    ;;
+
+  after-success)
+    run_command after-success
+    ;;
+
+  before-deploy)
+    run_command before-deploy
+    ;;
+
+  after-deploy)
+    run_command after-deploy
+    ;;
+esac
+
+exit $EXIT_VALUE
