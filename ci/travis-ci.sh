252fa266542f577ce94d62993a456d66d568b9f0
Mike Pirog <mike@kalamuna.com>
2016-05-01 00:47:22 -0700

    #1267: Deploy dev releases to S3

--- ci/travis-ci.sh
+++ ci/travis-ci.sh
@@ -78,31 +78,44 @@ before-deploy() {
 
   set -e
 
-  # Build things
-  BUILD_VERSION=${TRAVIS_TAG:-$TRAVIS_BRANCH}
-  echo $BUILD_VERSION
+  # THis is a production release!
+  if [ $TRAVIS_PULL_REQUEST == "false" ] &&
+    [ ! -z "$TRAVIS_TAG" ] &&
+    [ $TRAVIS_REPO_SLUG == "kalabox/kalabox" ]; then
+
+    # Get our prod version
+    BUILD_VERSION=${TRAVIS_TAG:-$TRAVIS_BRANCH}
+    echo $BUILD_VERSION
+
+    # cp dist/kalabox.dmg dist/kalabox-$BUILD_VERSION.dmg
+    # cp dist/kalabox.exe dist/kalabox-$BUILD_VERSION.exe
+    # cp dist/kalabox.deb dist/kalabox-$BUILD_VERSION.deb
+    # cp dist/kalabox.rpm dist/kalabox-$BUILD_VERSION.rpm
+
+    # Show me the money jerry
+    ls -lsa prod_build
+
+  fi
+
+  # Create a dev build dir
+  mkdir -p dev_build
 
   # Rename the builds and generate latest versions
   if [ $KALABOX_BUILD_PLATFORM == "osx" ]; then
-    mv dist/kalabox.dmg dist/kalabox-$BUILD_VERSION.dmg
-    cp dist/kalabox-$BUILD_VERSION.dmg dist/kalabox-latest.dmg
+    cp dist/kalabox.dmg dev_build/kalabox-latest.dmg
   fi
 
   if [ $KALABOX_BUILD_PLATFORM == "windows" ]; then
-    mv dist/kalabox.exe dist/kalabox-$BUILD_VERSION.exe
-    cp dist/kalabox-$BUILD_VERSION.exe dist/kalabox-latest.exe
+    cp dist/kalabox.exe dev_build/kalabox-latest.exe
   fi
 
   if [ $KALABOX_BUILD_PLATFORM == "linux" ]; then
-    mv dist/kalabox.deb dist/kalabox-$BUILD_VERSION.deb
-    cp dist/kalabox-$BUILD_VERSION.deb dist/kalabox-latest.deb
-
-    mv dist/kalabox.rpm dist/kalabox-$BUILD_VERSION.rpm
-    cp dist/kalabox-$BUILD_VERSION.rpm dist/kalabox-latest.rpm
+    cp dist/kalabox.deb dev_build/kalabox-latest.deb
+    cp dist/kalabox.rpm dev_build/kalabox-latest.rpm
   fi
 
   # Show me the money jerry
-  ls -lsa dist
+  ls -lsa dev_build
 
 }
 
