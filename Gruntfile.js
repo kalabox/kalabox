2c23faeafdc269f11e919ce1ffbf7ec8846403e5
Mike Pirog <mike@kalamuna.com>
2016-05-01 21:12:04 -0700

    Bump versions in preparation for v0.12.0-alpha.4 release

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,22 +18,21 @@ module.exports = function(grunt) {
   // setup task config
   var config = {
 
-    // This handles automatic version bumping
+    // This handles automatic version bumping in travis
     bump: {
       options: {
-        files: [
-          'package.json'
-        ],
+        files: ['package.json'],
         updateConfigs: [],
         commit: true,
         commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json', 'app/package.json'],
+        commitFiles: ['package.json'],
         createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
+        push: true,
         pushTo: 'origin',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-        globalReplace: false,
+        globalReplace: true,
         prereleaseName: 'alpha',
         metadata: '',
         regExp: false
