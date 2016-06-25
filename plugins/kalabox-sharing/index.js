b08426ee6995686631d7ddcfa323d1a386c4e38e
Mike Pirog <mike@kalamuna.com>
2016-06-24 22:45:41 -0700

    #1374: Basic unison implementation

--- plugins/kalabox-sharing/index.js
+++ plugins/kalabox-sharing/index.js
@@ -3,6 +3,4 @@
 module.exports = function(kbox) {
   // Load events for this plugin.
   require('./lib/events.js')(kbox);
-  // Load the share engine
-  require('./lib/share.js')(kbox);
 };
--- plugins/kalabox-sharing/lib/events.js
+++ plugins/kalabox-sharing/lib/events.js
@@ -13,6 +13,86 @@ module.exports = function(kbox) {
   var Promise = kbox.Promise;
 
   /*
+   * Helper function to generate the appropriate sharing compose file on
+   * WIN/OSX
+   */
+  var getSharingComposeNotLinux = function(app) {
+
+    // Get the remote service and webroot
+    var parts = app.config.sharing.share.split(':');
+    var webService = parts[0];
+    var webRoot = parts[1];
+
+    return {
+      unison: {
+        image: 'unison:test',
+        restart: 'always',
+        environment: {
+          'UNISON_WEBROOT': webRoot,
+          'UNISON_CODEROOT': '/src/' + app.config.sharing.codeDir,
+          'UNISON_OPTIONS': ''
+        },
+        volumes: [
+          '$KALABOX_APP_ROOT_BIND:/src'
+        ],
+        'volumes_from': [webService]
+      }
+    };
+  };
+
+  /*
+   * Helper function to generate the appropriate sharing compose file on
+   * Linux
+   */
+  var getSharingComposeLinux = function(app) {
+
+    // Start them up
+    var currentCompose = {};
+    var newCompose = {};
+
+    // Get the remote service and webroot
+    var parts = app.config.sharing.share.split(':');
+    var webService = parts[0];
+    var webRoot = parts[1];
+
+    // Get our composed things
+    _.forEach(app.composeCore, function(file)  {
+      _.extend(currentCompose, kbox.util.yaml.toJson(file));
+    });
+
+    // Add sync to correct container
+    _.forEach(currentCompose, function(value, key)  {
+      if (key === webService) {
+        var vol = [app.config.sharing.codeRoot, webRoot].join(':');
+        if (Array.isArray(value.volumes)) {
+          value.volumes.push(vol);
+        }
+        else {
+          value.volumes = [vol];
+        }
+        var obj = {};
+        obj[key] = {volumes: _.uniq(value.volumes)};
+        _.extend(newCompose, obj);
+      }
+    });
+
+    // Return our new compose
+    return newCompose;
+
+  };
+
+  /*
+   * Helper function to generate the appropriate sharing compose file
+   */
+  var getSharingCompose = function(app) {
+    switch (process.platform) {
+      case 'win32': return getSharingComposeNotLinux(app);
+      case 'darwin': return getSharingComposeNotLinux(app);
+      case 'linux': return getSharingComposeLinux(app);
+    }
+  };
+
+  /*
    * App events
    */
   kbox.core.events.on('post-app-load', function(app) {
@@ -55,79 +135,33 @@ module.exports = function(kbox) {
     app.config.sharing = config;
     delete app.config.pluginconfig.sharing;
 
-    /*
-     * Restart our shares
-     * When we start an app make sure we mount the share to the correct
-     * containers webroot
-     */
-    app.events.on('pre-start', 1, function() {
+    if (app.config.sharing.share) {
 
-      // Run through serializer.
-      app.status('Waiting for code sharing.');
+      // Make sure our local code root exist
+      return Promise.fromNode(function(cb) {
+        fs.mkdirp(app.config.sharing.codeRoot, cb);
+      })
 
-      if (app.config.sharing.share) {
+      // Create a sharing compose file and add it to the app
+      // correctly
+      .then(function() {
 
-        app.status('Sharing code.');
+        // Create dir to store this stuff
+        var tmpDir = path.join(kbox.util.disk.getTempDir(), app.name);
+        fs.mkdirpSync(tmpDir);
 
-        // Get the host code root
-        var codeRoot = app.config.sharing.codeRoot;
+        // Get our sharing compose file
+        var newCompose = getSharingCompose(app);
 
-        // @todo:
-        // Get the webroot and service on which it exists
+        if (!_.isEmpty(newCompose)) {
+          var fileName = [app.name, _.uniqueId()].join('-');
+          var newComposeFile = path.join(tmpDir, fileName + '.yml');
+          kbox.util.yaml.toYamlFile(newCompose, newComposeFile);
+          app.composeCore.push(newComposeFile);
+        }
 
-        // Make sure our code root exist
-        return Promise.fromNode(function(cb) {
-          fs.mkdirp(codeRoot, cb);
-        })
-
-        // @todo: Create a unison compose file and add it to the app
-        // correctly
-        .then(function() {
-
-          // Get our stuff from the sync config
-          var parts = app.config.sharing.share.split(':');
-          var webService = parts[0];
-          var webRoot = parts[1];
-
-          // Create dir to store this stuff
-          var tmpDir = path.join(kbox.util.disk.getTempDir(), app.name);
-          fs.mkdirpSync(tmpDir);
-
-          // Start them up
-          var currentCompose = {};
-          var newCompose = {};
-
-          // Get our composed things
-          _.forEach(app.composeCore, function(file)  {
-            _.extend(currentCompose, kbox.util.yaml.toJson(file));
-          });
-
-          // Add sync to correct container
-          _.forEach(currentCompose, function(value, key)  {
-            if (key === webService) {
-              var vol = [codeRoot, webRoot].join(':');
-              if (Array.isArray(value.volumes)) {
-                value.volumes.push(vol);
-              }
-              else {
-                value.volumes = [vol];
-              }
-              var obj = {};
-              obj[key] = {volumes: _.uniq(value.volumes)};
-              _.extend(newCompose, obj);
-            }
-          });
-
-          if (!_.isEmpty(newCompose)) {
-            var fileName = [app.name, _.uniqueId()].join('-');
-            var newComposeFile = path.join(tmpDir, fileName + '.yml');
-            kbox.util.yaml.toYamlFile(newCompose, newComposeFile);
-            app.composeCore.push(newComposeFile);
-          }
-
-        });
-      }
-    });
+      });
+    }
 
   });
 
