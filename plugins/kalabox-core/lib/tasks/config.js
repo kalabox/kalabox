b3a6a7caa6d17f282a9ccad70cbdef3adfdb6879
Mike Pirog <mike@kalabox.io>
2016-07-08 21:47:56 -0700

    #1372: Improve kbox env description and standarize other commands (#1442)
    * #1372: Improve kbox env description and standarize other commands
    
    * #1372: update changelog
    
    * #1372: Code styling fix

--- plugins/kalabox-core/lib/tasks/config.js
+++ plugins/kalabox-core/lib/tasks/config.js
@@ -9,7 +9,7 @@ module.exports = function(kbox) {
   // Prints out the config.
   kbox.tasks.add(function(task) {
     task.path = ['config'];
-    task.description = 'Display the kbox configuration.';
+    task.description = 'Display the kbox configuration';
     task.func = function() {
       var config = kbox.core.config.getGlobalConfig();
       console.log(JSON.stringify(config, null, '  '));
@@ -21,7 +21,7 @@ module.exports = function(kbox) {
       kbox.tasks.add(function(task) {
         task.path = [app.name, 'config'];
         task.category = 'appAction';
-        task.description = 'Display the kbox application\'s configuration.';
+        task.description = 'Display the kbox application\'s configuration';
         task.func = function() {
           console.log(JSON.stringify(app.config, null, '  '));
         };
