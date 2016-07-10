b343be58da9a762cab766e72a868f1d5df6d5523
Mike Pirog <mike@kalamuna.com>
2016-07-10 12:42:25 -0700

    #1440: Add ability to ignore or target unison paths

--- plugins/kalabox-sharing/dockerfiles/unison/run.sh
+++ plugins/kalabox-sharing/dockerfiles/unison/run.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-unison $UNISON_CODEROOT $UNISON_WEBROOT $UNISON_OPTIONS
+
+# Put our ignores and paths into arrays so we can correctly escape spaces
+IFS=';'
+IGNORES=( $UNISON_IGNORE )
+PATHS=( $UNISON_PATHS )
+unset IFS
+
+unison \
+  -root "${UNISON_CODEROOT}" \
+  -root "${UNISON_WEBROOT}" \
+  ${UNISON_OPTIONS} \
+  "${IGNORES[@]}" \
+  "${PATHS[@]}"
