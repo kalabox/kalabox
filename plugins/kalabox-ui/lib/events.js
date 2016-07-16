85dcb7956a81c06d2f076151ac2a87d579d2470d
Alec Reynolds <alec@kalamuna.com>
2016-07-15 18:27:41 -0700

    #1451: Deleting more cruft.

--- plugins/kalabox-ui/lib/events.js
+++ plugins/kalabox-ui/lib/events.js
@@ -1,15 +1,5 @@
 'use strict';
 
-/*
- * Plugin manages a utility container for each app called the token container.
- * If the token container is running then the app can be assumed to be
- * running, if it is stopped the app can be assumed to be stopped, and if the
- * container does not exist the app can be assumed to be uninstalled. This
- * allows app states and events/triggers to reach another process. For example
- * if the cli starts an app, the gui can receive a start event from this token
- * container. Another benefit is the container actions for the token take place
- * after all other containers in an app are done being acted upon.
- */
 module.exports = function(kbox) {
 
   /*
