bf619de6f42b931ced5c5a857c9b1eca73b00081
Mike Pirog <mike@kalamuna.com>
2016-08-02 11:58:17 -0700

    #955: Add force option to kbox start and stop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ v.13.0-beta.4
 * Loosened default `unison` ignore patterns. [#1501](https://github.com/kalabox/kalabox/issues/1501)
 * Bumped `stdin` max listeners to suppress `EventEmitter` warning. [#1502](https://github.com/kalabox/kalabox/issues/1502)
 * Better user feedback when running `kbox start` and `kbox stop`. [#955](https://github.com/kalabox/kalabox/issues/955)
+* Added a `force` option to `kbox start` and `kbox stop`. [#955](https://github.com/kalabox/kalabox/issues/955)
 
 v.13.0-beta.3
 =============
--- plugins/kalabox-core/lib/tasks/start.js
+++ plugins/kalabox-core/lib/tasks/start.js
@@ -13,6 +13,12 @@ module.exports = function(kbox) {
       kbox.tasks.add(function(task) {
         task.path = [app.name, 'start'];
         task.category = 'appAction';
+        task.options.push({
+          name: 'force',
+          alias: 'f',
+          kind: 'boolean',
+          description: 'Force start even if already running.'
+        });
         task.description = 'Start an installed kbox application.';
         task.func = function() {
 
@@ -20,12 +26,15 @@ module.exports = function(kbox) {
           var format = require('util').format;
           var log = kbox.core.log;
 
+          // Get options
+          var options = this.options || {};
+
           // Check to see if app is already running
           return kbox.app.isRunning(app)
 
           // Start if not running, otherwise inform user
           .then(function(isRunning) {
-            if (!isRunning) {
+            if (!isRunning || options.force) {
               return kbox.app.start(app)
               .then(function() {
                 console.log(kbox.art.appStart(app));
