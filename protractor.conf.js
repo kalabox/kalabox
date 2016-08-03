b4766a2ff0ba6ad822a3c09864b6755c15aa83b8
Mike Pirog <mike@kalabox.io>
2016-08-03 14:00:09 -0700

    #1456: Abstracted protractor config to handle plat AND arch

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,30 +1,61 @@
 'use strict';
 
-var platformDir = {
-  darwin: 'binary-nwjs-sdk-v0.14.6-osx-x64',
-  linux: 'binary-nwjs-sdk-v0.14.6-linux-x64',
-  win64: 'binary-nwjs-sdk-v0.14.6-win-x64'
-};
-var binary = {
-  darwin: 'nwjs.app/Contents/MacOS/nwjs',
-  linux: 'nw',
-  win64: 'nw.exe'
-};
+// Node modules
 var path = require('path');
-var platform = require('os').platform();
-var kbox = require('./lib/core/env.js');
-var nwbDir = path.join(kbox.getHomeDir(), '.nwjs-builder', 'caches',
-  platformDir[platform]);
-var nwBinary = path.join(nwbDir, binary[platform]);
-var chromedriver = path.join(nwbDir, 'chromedriver');
+var version = require('./tasks/nw.js')().pkg.options.version;
+
+/*
+ * Translate node process.platform into NWJS platform
+ */
+var getPlat = function() {
+  switch (process.platform) {
+    case 'win32': return 'win';
+    case 'darwin': return 'osx';
+    case 'linux': return 'linux';
+  }
+};
+
+/*
+ * Return the expected nwjs directory based on plat and arch
+ */
+var getNWDir = function() {
+
+  // Get some info before we start building
+  var arch = require('os').arch();
+  var homeDir = require('./lib/core/env.js').getHomeDir();
+
+  // Build the destination directory
+  var nwjsBaseDir = ['binary-nwjs-sdk-v' + version, getPlat(), arch].join('-');
+
+  // Return the nwjs base dir
+  return path.join(homeDir, '.nwjs-builder', 'caches', nwjsBaseDir);
+
+};
+
+/*
+ * Return the expected nwjs binary based on platform
+ */
+var getNWBinary = function() {
+
+  // Build a list of locations for us based on plat
+  var binary = {
+    osx: 'nwjs.app/Contents/MacOS/nwjs',
+    linux: 'nw',
+    win: 'nw.exe'
+  };
+
+  // Return the binary location
+  return path.join(getNWDir(), binary[getPlat()]);
+
+};
 
 exports.config = {
-  chromeDriver: chromedriver,
+  chromeDriver: path.join(getNWDir(), 'chromedriver'),
   chromeOnly: true,
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      binary: nwBinary,
+      binary: getNWBinary(),
       args: ['nwapp=build/gui']
     }
   },
