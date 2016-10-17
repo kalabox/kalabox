791b2a3f2427eccbd73594c7db74f6b5fcb41bc7
Alec Reynolds <alec@kalamuna.com>
2016-10-17 11:40:44 -0700

    #1607: Workaround to fix documentation search.

---
+++ docs/js/extra.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+  fixSearch();
+});
+
+function fixSearch() {
+  var target = document.getElementById('rtd-search-form');
+  var config = {attributes: true, childList: true};
+
+  var observer = new MutationObserver(function(mutations) {
+    // if it isn't disconnected it'll loop infinitely because the observed element is modified
+    observer.disconnect();
+    var form = $('#rtd-search-form');
+    form.empty();
+    form.attr('action', 'https://' + window.location.hostname + '/en/' + determineSelectedBranch() + '/search.html');
+    $('<input>').attr({
+      type: "text",
+      name: "q",
+      placeholder: "Search docs"
+    }).appendTo(form);
+  });
+  // don't run this outside RTD hosting
+  if (window.location.origin.indexOf('readthedocs') > -1) {
+    observer.observe(target, config);
+  }
+}
+
+/**
+ * Analyzes the URL of the current page to find out what the selected GitHub branch is. It's usually
+ * part of the location path. The code needs to distinguish between running MkDocs standalone
+ * and docs served from RTD. If no valid branch could be determined 'dev' returned.
+ *
+ * @returns GitHub branch name
+ */
+function determineSelectedBranch() {
+  var branch = 'dev', path = window.location.pathname;
+  if (window.location.origin.indexOf('readthedocs') > -1) {
+    // path is like /en/<branch>/<lang>/build/ -> extract 'lang'
+    // split[0] is an '' because the path starts with the separator
+    branch = path.split('/')[2];
+  }
+  return branch;
+}
